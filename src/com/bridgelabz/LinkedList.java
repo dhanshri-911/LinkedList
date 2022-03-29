2a949d1a08b81b1513c971473c7f3f71aba939f3
Dhanshri911 <dszingade911@gmail.com>
2022-03-30 00:28:17 +0530

    UC5 : Delete First Element

--- src/com/bridgelabz/LinkedList.java
+++ src/com/bridgelabz/LinkedList.java
@@ -1,19 +1,33 @@
 package com.bridgelabz;
 
-public class LinkedList {
-    Node head;
-    Node tail;//head
+public class  LinkedList<T> {
+    Node<T> head;
+    Node <T> tail;//head
 
     public void printList() {
-        Node temp = head;
+        Node <T> temp = head;
         while (temp != null){
             System.out.println(temp.data+" ");
             temp=temp.next;
         }
     }
-
-    public void add(int data) {
-        Node newNode = new Node(data);
+    public void insertAfter(Node<T> previousNode,T data){
+        if(previousNode == null){
+            System.out.println("previous Node cant be null");
+            return;
+        }
+        Node <T> newNode = new Node <T>(data);
+        newNode.next = previousNode.next;
+        previousNode.next = newNode;
+    }
+     public void pop(){
+        if(head == null){
+            System.out.println("List is empty");
+        }
+        head = head.next;
+    }
+    public void add(T data) {
+        Node<T> newNode = new Node<T>(data);
         if (head == null) {
             head = newNode;
             tail = newNode;
@@ -22,8 +36,8 @@ public class LinkedList {
             tail = newNode;
         }
     }
-    public void push(int data) {
-        Node newNode = new Node(data);
+    public void push(T data) {
+        Node <T> newNode = new Node<T>(data);
         if (head == null){
             head = newNode;
             tail = newNode;
@@ -33,4 +47,5 @@ public class LinkedList {
             head = newNode;
         }
     }
+
 }
--- src/com/bridgelabz/MainClass.java
+++ src/com/bridgelabz/MainClass.java
@@ -2,17 +2,30 @@ package com.bridgelabz;
 
 public class MainClass {
     public static void main(String[] args) {
-        LinkedList list = new LinkedList();
+        LinkedList<Integer> list = new LinkedList<>();
         //System.out.println("----------------UC1----------------");
         //list.add(56);
         //list.add(30);
         //list.add(70);
         //list.printList();
 
-        System.out.println("----------------UC2----------------");
-        list.push(70);
-        list.push(30);
-        list.push(56);
+        //System.out.println("----------------UC2----------------");
+       // list.push(70);
+       // list.push(30);
+       // list.push(56);
+       // list.printList();
+
+        System.out.println("----------------UC4----------------");
+        //list.add(56);
+        //list.add(70);
+        //list.insertAfter(list.head,30);
+        //list.printList();
+
+        System.out.println("----------------UC5----------------");
+        list.add(56);
+        list.add(30);
+        list.add(70);
+        list.pop();
         list.printList();
 
     }
