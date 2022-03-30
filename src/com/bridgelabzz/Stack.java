e732f4f16f255c39bb5bd27ef4e3732a828b22fc
Dhanshri911 <dszingade911@gmail.com>
2022-03-30 23:47:27 +0530

    UC2 :Added Stack Pop peek Using LinkedList

--- src/com/bridgelabzz/Stack.java
+++ src/com/bridgelabzz/Stack.java
@@ -12,5 +12,22 @@ public class Stack {
         list.push(30);
         list.push(56);
         list.printList();
+
+        System.out.println("________UC2_________");
+        System.out.println("Before Pop");
+        System.out.println("Size " +list.size());
+        list.printList();
+        while(list.size() != 0){
+            list.pop();
+        }
+        System.out.println("Aftr Pop");
+        list.printList();
+        System.out.println("the stack is Empty");
+        list.push(70);
+        list.push(30);
+        list.push(56);
+        //list.printList();
+        list.peek();
+        list.printList();
     }
 }
